0c7316dbe1f073093aee5091268d913f1ef16683
Qirong Wang <qirongwang@umass.edu>
2025-12-30 16:52:13 +0800

    first chapter

--- DML.java
+++ DML.java
@@ -10,4 +10,32 @@ ALTER TABLE person CHANGE age age INT(3) DEFAULT '18';
 
 INSERT INTO person VALUES ('ABCD',34);//SAME ORDER AS TABLE ABOVE.
 INSERT INTO person VALUES ('EFGH',12);
+INSERT INTO person VALUES (DEFAULT,55);//insert default value if it has.
+INSERT INTO person VALUES (NULL,66);// or null.
+
+INSERT INTO person 
+VALUES('AAA',22),('BBB',33),('CCC',44);
+
+
+UPDATE person SET age=40;//this will change all age to 40!
+
+UPDATE person 
+SET age = 22
+WHERE name = 'AAA';
+//!= IS <> IN SQL NOT != IN JAVA.
+
+UPDATE person 
+SET age = 39
+WHERE age > 30;
+
+UPDATE person 
+SET age = 66, name = 'DDD'
+WHERE name = 'CCC';
+
+
+DELETE FROM person
+WHERE name = 'DDD';
+
+DELETE FROM person 
+WHERE age < 30;
 */
