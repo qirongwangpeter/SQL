993172604136578fccb3d31b4a5e352c3b45f6f9
Qirong Wang <qirongwang@umass.edu>
2026-01-04 16:35:55 +0800

    Added more

--- ThirdDeepCheckingAndFunction.sql
+++ ThirdDeepCheckingAndFunction.sql
@@ -8,3 +8,33 @@ SELECT COUNT(name)
 FROM teacher;
 
 #functions ignor null values
+
+SELECT AVG(comm) FROM teacher;
+SELECT IFNULL(comm,0) FROM teacher;
+SELECT AVG(IFNULL(comm,0)) FROM teacher;
+
+
+SELECT COUNT(1) FROM teacher;
+SELECT COUNT(*) FROM teacher;
+
+# GROUP BY
+SELECT AVG(salary),tile
+FROM teacher
+GROUP BY title
+
+#同班级同性人数多少
+
+SELECT COUNT(*)
+FROM student
+GROUP BY class_id, gender;
+
+#每个班的每种职位多少人？
+SELECT COUNT(*)
+FROM student
+GROUP BY class_id,job; 
+
+# ORDER BY
+SELECT AVG(salary) avg_sal,subject_id
+FROM teacher
+GROUP BY subject_id
+ORDER BY avg_sal DESC;
