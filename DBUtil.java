f495098939492b16fb75e017be532d9358e3ea87
Qirong Wang <qirongwang@umass.edu>
2026-01-07 21:10:54 +0800

    added utility for DBUtil

---
+++ DBUtil.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    static {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3307/tedu?characterEncoding=utf8&useSSL=false&serverTimezone=Asia/Shanghai&rewriteBatchedStatements=true",
+                "root",
+                "root");
+    }
+}
